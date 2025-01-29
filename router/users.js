596c00a9867e325e89123f9885005b8172c39a51
intanadeliaa <132634408+intanadelia22@users.noreply.github.com>
2025-01-29 17:58:00 +0700

    modifikasi users

--- router/users.js
+++ router/users.js
@@ -5,21 +5,27 @@ const users = [
     {id: 1, nama: "Intan", email: "liannavic08@gmail.com"},
     {id: 2, nama: "Adelia", email: "alaia22@gmail.com"},
 ]
+
+// GET semua user
 router.get('/users', (req, res) => {
     res.json(users)
-  })
-  
-  router.post('/user', (req, res) => {
-    res.send('Got a POST request')
-  })
-  
-  router.put('/user/:id', (req, res) => {
+})
+
+// POST user baru
+router.post('/user', (req, res) => {
+    users.push(req.body)
+    res.json(users)
+})
+
+// PUT (update) user berdasarkan ID
+router.put('/user/:id', (req, res) => {
     res.send('Got a PUT request at /user')
-  })
-  
-  router.delete('/user/:id', (req, res) => {
+})
+
+// DELETE user berdasarkan ID
+router.delete('/user/:id', (req, res) => {
     res.send('Got a DELETE request at /user')
-  })
-  
-  module.exports = router
-  
+})
+
+// Ekspor router agar bisa digunakan di index.js
+module.exports = router
